2998e720cf81f3d97816c4f63edad691e3a344a6
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-08-31 13:49:26 +0900

    feat(MAM-6): 카카오 맵 개발 환경 설정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,21 @@
 import { css } from "@emotion/react";
+import { useEffect, useRef } from "react";
 
 const MainPage = () => {
-  return <div css={css({ color: "red" })}>mainPage</div>;
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!mapRef.current) return;
+
+    var options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    var map = new kakao.maps.Map(mapRef.current, options); //지도 생성 및 객체 리턴
+  }, []);
+  return <div ref={mapRef} css={css({ width: "100dvw", height: "100dvh" })} />;
 };
 
 export default MainPage;
