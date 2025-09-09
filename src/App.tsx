b09b222c8f3f2ad9c9af4f4cc0012c4fe6695407
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-09-10 00:24:38 +0900

    feat(MAM-10): OverlayProvider를 사용하여 라우터에 Outlet 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { Global } from "@emotion/react";
 import globalStyles from "./styles/globalStyles";
 import MainPage from "./pages/main";
 import RestaurantDetailPage from "./pages/restaurant";
+import { OverlayProvider } from "overlay-kit";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: (
+      <OverlayProvider>
+        <Outlet />
+      </OverlayProvider>
+    ),
     children: [
       {
         path: "/",
