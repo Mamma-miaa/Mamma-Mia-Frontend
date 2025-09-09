157ff3efa557c3d0f24e77790bb5e8e15b68789d
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-09-10 00:26:21 +0900

    feat(MAM-12): 레스토랑 리스트 팝업 컴포넌트 추가 및 필터 기능 구현

---
+++ src/pages/main/_components/RestaurantListPopup.tsx
@@ -0,0 +1,88 @@
+import Spacing from "@/@lib/components/Spacing";
+import THEME from "@/constants/theme";
+import { css } from "@emotion/react";
+import NewIcon from "../_assets/new.svg?react";
+import ResponsiveSummaryCard from "./ResponsiveSummaryCard";
+import { useNavigate } from "react-router-dom";
+
+const RestaurantListPopup = ({
+  data,
+}: {
+  data: {
+    id: string;
+    restaurantName: string;
+    category: string;
+    address: string;
+    latitude: number;
+    longitude: number;
+    restaurantImageUrl: string;
+    distance: string;
+  }[];
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <div css={popupContainerStyle}>
+      <Spacing size={20} />
+      {/* 필터 칩 컨테이너 */}
+      <div css={filterContainerStyle}>
+        <div css={css(filterChipStyle)}>
+          <NewIcon />
+        </div>
+
+        {["영업중", "카테고리", "가격대"].map((text) => (
+          <div css={css(filterChipStyle)}>{text}</div>
+        ))}
+      </div>
+      <Spacing size={16} />
+      <div
+        css={css({
+          display: "flex",
+          flexDirection: "column",
+          padding: "0 20px",
+        })}
+      >
+        {data.map((restaurant) => (
+          <ResponsiveSummaryCard
+            {...restaurant}
+            key={restaurant.id}
+            onClick={() => navigate(`/restaurant?id=${restaurant.id}`)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const popupContainerStyle = css({
+  width: "100dvw",
+  height: "100dvh",
+  position: "fixed",
+  top: 0,
+  left: 0,
+  backgroundColor: THEME.COLORS.BACKGROUND.WHITE,
+  zIndex: 1000,
+});
+
+const filterContainerStyle = css({
+  display: "flex",
+  alignItems: "center",
+  gap: 8,
+  padding: "0 20px",
+  height: 44,
+});
+
+const filterChipStyle = css({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "8px 16px",
+  height: 44,
+  backgroundColor: THEME.COLORS.BACKGROUND.WHITE,
+  border: `1px solid ${THEME.COLORS.LINE.NORMAL}`,
+  borderRadius: 1000,
+  cursor: "pointer",
+  transition: "all 0.2s ease",
+});
+
+export default RestaurantListPopup;
