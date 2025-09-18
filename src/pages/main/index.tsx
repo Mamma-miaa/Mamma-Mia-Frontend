0b64cd169fc3f575f4288459140dcab078f3d8e8
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-09-19 00:15:38 +0900

    feat(MAM-24): 상수 파일 분리 및 코드 정리
    - 충무로역 좌표, 딤 영역, 서비스 영역을 새로운 상수 파일로 분리하여 코드 가독성 향상
    - 메인 페이지에서 불필요한 상수 제거 및 import 방식 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -19,54 +19,7 @@ import TopNavigation from "./_components/TopNavigation";
 import { useGetNearbyStoreQuery } from "@/hooks/@server/store";
 import VIEWPORT from "@/constants/viewport";
 import PopupToggleButton from "./_components/PopupToggleButton";
-
-const 충무로역_좌표 = {
-  lat: 37.561306,
-  lng: 126.9945,
-};
-
-const 딤_영역 = [
-  new kakao.maps.LatLng(0.0, 200.0),
-  new kakao.maps.LatLng(200.0, 0.0),
-  new kakao.maps.LatLng(200.0, 200.0),
-  new kakao.maps.LatLng(0.0, 0.0),
-];
-
-const 서비스_영역 = [
-  new kakao.maps.LatLng(37.570294707575194, 126.99218948837137),
-  new kakao.maps.LatLng(37.57052912439423, 126.99513256445218),
-  new kakao.maps.LatLng(37.570979706332714, 127.00194698594886),
-  new kakao.maps.LatLng(37.56878127856788, 127.00187901260531),
-  new kakao.maps.LatLng(37.566889180958036, 127.0022524942787),
-  new kakao.maps.LatLng(37.56639350683301, 127.00590851367807),
-  new kakao.maps.LatLng(37.565996955862914, 127.007844019678),
-  new kakao.maps.LatLng(37.56417698194663, 127.0072439400642),
-  new kakao.maps.LatLng(37.562203840054615, 127.00655332918932),
-  new kakao.maps.LatLng(37.5601946640645, 127.00564770890021),
-  new kakao.maps.LatLng(37.55900534365135, 127.00573816212346),
-  new kakao.maps.LatLng(37.55650062164743, 127.00458358554847),
-  new kakao.maps.LatLng(37.5552302409573, 127.00385920034324),
-  new kakao.maps.LatLng(37.55363550947771, 127.00259160717565),
-  new kakao.maps.LatLng(37.552241228434575, 127.0018842557036),
-  new kakao.maps.LatLng(37.55011038203629, 126.99967182066207),
-  new kakao.maps.LatLng(37.54909225340014, 127.00041870587384),
-  new kakao.maps.LatLng(37.54717760419666, 127.00255177728496),
-  new kakao.maps.LatLng(37.54596575992392, 127.00258002573237),
-  new kakao.maps.LatLng(37.54380336989664, 127.00217259049397),
-  new kakao.maps.LatLng(37.54095170602315, 126.99782749225348),
-  new kakao.maps.LatLng(37.54258235939297, 126.99355022716232),
-  new kakao.maps.LatLng(37.54285252268164, 126.99140592957117),
-  new kakao.maps.LatLng(37.544478800614904, 126.99105495688413),
-  new kakao.maps.LatLng(37.54619053557534, 126.98910841131594),
-  new kakao.maps.LatLng(37.546064245515495, 126.98758643883173),
-  new kakao.maps.LatLng(37.554713321954125, 126.9834429000457),
-  new kakao.maps.LatLng(37.557704609695236, 126.9832724730448),
-  new kakao.maps.LatLng(37.56033534503654, 126.98207216896988),
-  new kakao.maps.LatLng(37.56197503611494, 126.98127949345901),
-  new kakao.maps.LatLng(37.566047751895546, 126.98267070383284),
-  new kakao.maps.LatLng(37.566165502609785, 126.98760574186248),
-  new kakao.maps.LatLng(37.57026504120442, 126.98768429960681),
-];
+import { 충무로역_좌표, 딤_영역, 서비스_영역 } from "./_constants";
 
 const MainPage = () => {
   const mapRef = useRef<HTMLDivElement>(null);
