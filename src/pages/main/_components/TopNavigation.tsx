942a62881d707493843b494c4e2817e2fc4b06b1
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-09-22 20:18:26 +0900

    feat: TopNavigation 컴포넌트에 버튼 클릭 시 토스트 메시지 추가
    - 각 버튼에 클릭 이벤트를 추가하여 "현재 개발이 필요한 기능입니다."라는 메시지를 표시하도록 수정함.

--- src/pages/main/_components/TopNavigation.tsx
+++ src/pages/main/_components/TopNavigation.tsx
@@ -5,18 +5,34 @@ import styled from "@emotion/styled";
 import challengeImg from "@/assets/emoji/challenge.webp";
 import medalImg from "@/assets/emoji/medal.webp";
 import personImg from "@/assets/emoji/person.webp";
+import toast from "@/utils/toast";
 
 const TopNavigation = () => {
   return (
     <div css={buttonContainerStyle}>
-      <Button>
+      <Button
+        type="button"
+        onClick={() => {
+          toast({ message: "현재 개발이 필요한 기능입니다." });
+        }}
+      >
         <img src={challengeImg} alt="도전맛집" width={20} height={20} />
         도전맛집
       </Button>
-      <Button>
+      <Button
+        type="button"
+        onClick={() => {
+          toast({ message: "현재 개발이 필요한 기능입니다." });
+        }}
+      >
         <img src={medalImg} alt="랭킹" width={20} height={20} /> 랭킹
       </Button>
-      <Button>
+      <Button
+        type="button"
+        onClick={() => {
+          toast({ message: "현재 개발이 필요한 기능입니다." });
+        }}
+      >
         <img src={personImg} alt="MY" width={20} height={20} /> MY
       </Button>
     </div>
