33166984e0b8b344f3d789e123b99a26bc5dbbc1
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-09-30 00:50:06 +0900

    feat(MAM-28): 최근 검색어 섹션의 상태 관리 및 UI 개선
    - 최근 검색어 섹션에 검색어가 없을 경우 메시지를 표시하도록 상태 관리 기능 추가.
    - 전체삭제 버튼을 검색어가 있을 때만 표시하도록 조건부 렌더링 구현.
    - UI 요소의 스타일을 조정하여 사용자 경험을 향상시킴.

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -6,9 +6,11 @@ import THEME from "@/constants/theme";
 import TYPOGRAPHY from "@/constants/typography";
 import RemoveIcon from "./_assets/remove.svg?react";
 import ArrowDownIcon from "./_assets/arrow_down.svg?react";
+import { useState } from "react";
 
 const SearchPage = () => {
   const navigate = useNavigate();
+  const [isEmpty] = useState(true);
 
   return (
     <div css={css({ width: "100%", height: "100dvh" })}>
@@ -28,27 +30,39 @@ const SearchPage = () => {
       <div css={recentSearchSectionStyle}>
         <div css={recentSearchHeaderStyle}>
           <h2 css={recentSearchTitleStyle}>최근 검색어</h2>
-          <button css={deleteAllButtonStyle}>전체삭제</button>
+          {!isEmpty && <button css={deleteAllButtonStyle}>전체삭제</button>}
         </div>
 
         <div css={recentSearchListStyle}>
-          <div css={recentSearchItemStyle}>
-            <span css={recentSearchTextStyle}>검색1</span>
-            <RemoveIcon />
-          </div>
-          <div css={recentSearchItemStyle}>
-            <span css={recentSearchTextStyle}>검색1</span>
-            <RemoveIcon />
-          </div>
-          <div css={recentSearchItemStyle}>
-            <span css={recentSearchTextStyle}>검색1</span>
-            <RemoveIcon />
-          </div>
-
-          <div css={showMoreStyle}>
-            <span css={showMoreTextStyle}>최근 검색어 더보기</span>
-            <ArrowDownIcon />
-          </div>
+          {isEmpty ? (
+            <p
+              css={css(TYPOGRAPHY.BODY["14R"], {
+                color: THEME.COLORS.GRAYSCALE.ALTERNATIVE,
+                marginTop: 97,
+              })}
+            >
+              최근 검색어 내역이 없습니다.
+            </p>
+          ) : (
+            <>
+              <div css={recentSearchItemStyle}>
+                <span css={recentSearchTextStyle}>검색1</span>
+                <RemoveIcon />
+              </div>
+              <div css={recentSearchItemStyle}>
+                <span css={recentSearchTextStyle}>검색1</span>
+                <RemoveIcon />
+              </div>
+              <div css={recentSearchItemStyle}>
+                <span css={recentSearchTextStyle}>검색1</span>
+                <RemoveIcon />
+              </div>
+              <div css={showMoreStyle}>
+                <span css={showMoreTextStyle}>최근 검색어 더보기</span>
+                <ArrowDownIcon />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
@@ -100,6 +114,7 @@ const recentSearchListStyle = css({
   flexDirection: "column",
   alignItems: "center",
   width: "100%",
+  height: 276,
 });
 
 const recentSearchItemStyle = css({
@@ -118,19 +133,6 @@ const recentSearchTextStyle = css(
   TYPOGRAPHY.BODY["14R"]
 );
 
-const deleteButtonStyle = css({
-  width: 20,
-  height: 20,
-  backgroundColor: "transparent",
-  border: "none",
-  cursor: "pointer",
-  color: THEME.COLORS.GRAYSCALE.ALTERNATIVE,
-  fontSize: 16,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-});
-
 const showMoreStyle = css({
   display: "flex",
   justifyContent: "center",
@@ -146,10 +148,5 @@ const showMoreTextStyle = css(
   },
   TYPOGRAPHY.BODY["14R"]
 );
-
-const arrowIconStyle = css({
-  color: THEME.COLORS.GRAYSCALE.ALTERNATIVE,
-  fontSize: 12,
-});
 
 export default SearchPage;
