1b183d1ab883036227d6743bc0f88620e9b9af5a
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-10-07 00:25:33 +0900

    feat(MAM-41): 마이 페이지 레이아웃 및 컴포넌트 구성 개선
    - 마이 페이지에 새로운 레이아웃을 추가하고, 프로필 섹션 및 도전맛집 현황 섹션을 포함하여 사용자 경험을 향상시킴.
    - 다크 모드에 적합한 뒤로가기 아이콘과 제목 이미지를 추가하여 시각적 요소를 개선함.

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,5 +1,25 @@
+import BackIcon from "@/@lib/assets/back_dark.svg?react";
+import Spacing from "@/@lib/components/Spacing";
+import { css } from "@emotion/react";
+import myTitleImage from "./_assets/my_text.png";
+import ProfileSection from "./_components/ProfileSection";
+import MyChallengeStatusSection from "./_components/MyChallengeStatusSection";
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div css={css({ width: "100%", height: "100dvh" })}>
+      <Spacing size={20} />
+      <BackIcon css={css({ marginLeft: 20 })} />
+      <Spacing size={16} />
+      <div css={css({ display: "flex", justifyContent: "center" })}>
+        <img src={myTitleImage} alt="my title" width={58} height={43} />
+      </div>
+      <Spacing size={20} />
+      <ProfileSection />
+      <Spacing size={20} />
+      <MyChallengeStatusSection />
+    </div>
+  );
 };
 
 export default MyPage;
