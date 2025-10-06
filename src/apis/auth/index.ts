8f6917fe743a2ccd5de0354754843bc0841f6bfd
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-10-07 01:04:28 +0900

    feat(MAM-29): 소셜 로그인 API 응답 타입 및 로그인 리다이렉트 개선
    - `postSocialLogin` 함수의 반환 타입을 `JoinSocialLoginResponse`로 명시하여 타입 안정성을 향상시킴.
    - 로그인 리다이렉트 페이지에서 `onSuccess` 콜백의 매개변수를 간소화하여 코드 가독성을 개선함.

--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -3,6 +3,6 @@ import type { components } from "../schema";
 
 export const postSocialLogin = async (
   data: components["schemas"]["JoinSocialLoginRequest"]
-) => {
+): Promise<components["schemas"]["JoinSocialLoginResponse"]> => {
   return await api.post(`/auth/social`, data);
 };
