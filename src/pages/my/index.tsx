69b24156afde650341d83527bb486778ac810e02
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-10-07 00:26:50 +0900

    feat(MAM-41): 마이 페이지에서 뒤로가기 아이콘 클릭 시 이전 페이지로 이동 기능 추가
    - 뒤로가기 아이콘에 클릭 이벤트를 추가하여 사용자가 이전 페이지로 쉽게 이동할 수 있도록 구현함.
    - `react-router-dom`의 `useNavigate` 훅을 사용하여 내비게이션 기능을 추가함.

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -4,12 +4,15 @@ import { css } from "@emotion/react";
 import myTitleImage from "./_assets/my_text.png";
 import ProfileSection from "./_components/ProfileSection";
 import MyChallengeStatusSection from "./_components/MyChallengeStatusSection";
+import { useNavigate } from "react-router-dom";
 
 const MyPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div css={css({ width: "100%", height: "100dvh" })}>
       <Spacing size={20} />
-      <BackIcon css={css({ marginLeft: 20 })} />
+      <BackIcon onClick={() => navigate(-1)} css={css({ marginLeft: 20 })} />
       <Spacing size={16} />
       <div css={css({ display: "flex", justifyContent: "center" })}>
         <img src={myTitleImage} alt="my title" width={58} height={43} />
