c8055943c74deec0c50c3746fd9689ff117d146d
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-10-28 21:37:34 +0900

    feat(MAM-28): useSearchInput 훅에 초기값 설정 기능 추가
    - useSearchInput 훅에 initialValue 파라미터를 추가하여 초기 입력 값을 설정할 수 있도록 수정함.
    - 기본값은 빈 문자열로 설정되며, 이를 통해 검색 입력 상태 관리를 개선함.

--- src/pages/search/_hooks/useSearchInput.ts
+++ src/pages/search/_hooks/useSearchInput.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-const useSearchInput = () => {
-  const [inputValue, setInputValue] = useState("");
+const useSearchInput = ({ initialValue }: { initialValue?: string } = {}) => {
+  const [inputValue, setInputValue] = useState(initialValue || "");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
