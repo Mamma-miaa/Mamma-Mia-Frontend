10c780705fa000c2b441f3e5c53ca5b65092fda2
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-11-09 13:03:27 +0900

    feat(MAM-55): 등록 중단 확인 모달 기능 추가
    - 등록 중단 시 사용자에게 확인 모달을 표시하도록 기능을 추가하였습니다.
    - 사용자가 등록을 중단할 경우 작성한 내용이 사라진다는 경고 메시지를 포함하였습니다.
    - UI의 일관성을 유지하기 위해 ExitIcon 클릭 시 새로운 핸들러를 적용하였습니다.

--- src/pages/challenge/registration/_components/ChallengeRegistrationPageHeader.tsx
+++ src/pages/challenge/registration/_components/ChallengeRegistrationPageHeader.tsx
@@ -3,15 +3,28 @@ import ExitIcon from "../_assets/exit.svg?react";
 import registrationTitle from "../_assets/registration.webp";
 import Spacing from "@/@lib/components/Spacing";
 import { css } from "@emotion/react";
+import { openConfirmModal } from "@/components/ConfirmModal/utils";
 
 const ChallengeRegistrationPageHeader = () => {
   const navigate = useNavigate();
+
+  const handleExit = async () => {
+    const isOk = await openConfirmModal({
+      title: "등록을 중단하시겠습니까?",
+      description: "등록을 중단하면 작성한 내용이 모두 사라집니다.",
+      cancelText: "나가기",
+      confirmText: "계속 등록하기",
+    });
+    if (isOk) {
+      navigate(-1);
+    }
+  };
 
   return (
     <>
       {/* 헤더 */}
       <div css={css({ padding: "16px 20px" })}>
-        <ExitIcon onClick={() => navigate(-1)} />
+        <ExitIcon onClick={handleExit} />
       </div>
       {/* 제목 */}
       <Spacing size={20} />
