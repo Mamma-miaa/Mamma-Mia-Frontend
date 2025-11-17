8c62f559cb9c96bc2e7f2df70579335d737da952
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-11-17 22:18:09 +0900

    feat(MAM-68): 숫자 입력을 위한 커스텀 훅 추가
    - 숫자 입력을 처리하는 useNumberInput 훅을 새로 추가하였습니다.
    - 입력값이 숫자인지 확인하고 상태를 관리하는 기능을 구현하였습니다.
    - RecommendedMenuRegisterBottomSheet 컴포넌트에서 useTextInput 대신 useNumberInput을 사용하도록 수정하였습니다.

---
+++ src/hooks/useNumberInput.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const useNumberInput = ({ initialValue }: { initialValue?: number } = {}) => {
+  const [value, setValue] = useState(initialValue || 0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    if (newValue.match(/^\d*$/)) {
+      setValue(Number(newValue));
+    }
+  };
+
+  return { value, handleChange };
+};
+
+export default useNumberInput;
