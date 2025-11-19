9a6db3002d9c74b1cc7400bcca561c81d4e4aafd
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-11-19 22:46:38 +0900

    feat(MAM-72): 서비스 약관 동의 팝업 컴포넌트 추가
    - 서비스 약관 동의 팝업을 ModalContainer를 사용하여 구현하였습니다.
    - 전체 동의 및 개별 동의 기능을 추가하였으며, 동의 여부에 따라 버튼 활성화 로직을 구현하였습니다.
    - 관련 SVG 아이콘 파일을 추가하여 UI를 개선하였습니다.
    - 스토리북을 통해 컴포넌트를 문서화하였습니다.

--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -1,8 +1,13 @@
+import type { AxiosRequestConfig } from "axios";
 import { api } from "../instance";
 import type { components } from "../schema";
 
-export const postSocialLogin = async (
-  data: components["schemas"]["JoinSocialLoginRequest"]
-): Promise<components["schemas"]["JoinSocialLoginResponse"]> => {
-  return await api.post(`/auth/social`, data);
+export const postSocialLogin = async ({
+  data,
+  config,
+}: {
+  data: components["schemas"]["JoinSocialLoginRequest"];
+  config?: AxiosRequestConfig;
+}): Promise<components["schemas"]["JoinSocialLoginResponse"]> => {
+  return await api.post(`/auth/social`, data, config);
 };
