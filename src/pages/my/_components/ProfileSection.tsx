198db1bc5d3e66640695f1fe97f2f0610fe1f207
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-11-21 22:27:22 +0900

    feat(MAM-73): 프로필 섹션에 아이콘 추가 및 레이아웃 수정
    - 로그아웃 및 프로필 편집 버튼에 새로운 SVG 아이콘을 추가하였습니다.
    - 프로필 섹션의 레이아웃을 개선하여 사용자 경험을 향상시켰습니다.
    - 기존의 화살표 아이콘을 제거하고, 새로운 아이콘으로 대체하였습니다.

--- src/pages/my/_components/ProfileSection.tsx
+++ src/pages/my/_components/ProfileSection.tsx
@@ -1,36 +1,32 @@
 import { css } from "@emotion/react";
 import THEME from "@/constants/theme";
 import TYPOGRAPHY from "@/constants/typography";
-import ArrowIcon from "../_assets/arrow_right.svg?react";
+import WriteIcon from "../_assets/write.svg?react";
+import LogoutIcon from "../_assets/logout.svg?react";
 
 const ProfileSection = () => {
   return (
     <section css={css({ padding: "0 20px" })}>
       <div css={containerStyle}>
-        <div css={profileRowStyle}>
-          <div css={profileLeftStyle}>
+        <div css={topSectionStyle}>
+          <div css={profileRowStyle}>
             <div css={avatarStyle} />
-            <div>
+            <div css={profileInfoStyle}>
               <div css={nameStyle}>Nickname</div>
               <div css={joinedStyle}>2025.10.06 가입</div>
             </div>
           </div>
-          <ArrowIcon width={24} height={24} />
         </div>
 
-        <div css={statsRowStyle}>
-          <div css={statColStyle}>
-            <div css={statNumberStyle}>12</div>
-            <div css={statLabelStyle}>Mamma-Mia!</div>
-          </div>
-          <div css={statColStyle}>
-            <div css={statNumberStyle}>12</div>
-            <div css={statLabelStyle}>Cheer-UP!</div>
-          </div>
-          <div css={statColStyle}>
-            <div css={statNumberStyle}>12</div>
-            <div css={statLabelStyle}>승인된 도전맛집 수</div>
-          </div>
+        <div css={bottomSectionStyle}>
+          <button css={actionButtonStyle}>
+            <WriteIcon width={20} height={20} />
+            <span css={buttonTextStyle}>프로필 편집</span>
+          </button>
+          <button css={[actionButtonStyle, logoutButtonStyle]}>
+            <LogoutIcon width={20} height={20} />
+            <span css={buttonTextStyle}>로그아웃</span>
+          </button>
         </div>
       </div>
     </section>
@@ -43,21 +39,21 @@ const containerStyle = css({
   alignSelf: "center",
   backgroundColor: THEME.COLORS.GRAYSCALE.NORMAL,
   borderRadius: 12,
-  padding: 12,
   display: "flex",
   flexDirection: "column",
+  overflow: "hidden",
+});
+
+const topSectionStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignSelf: "stretch",
   gap: 16,
+  padding: "20px 12px",
 });
 
 const profileRowStyle = css({
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-  justifyContent: "space-between",
-  alignSelf: "stretch",
-});
-
-const profileLeftStyle = css({
   display: "flex",
   flexDirection: "row",
   alignItems: "center",
@@ -68,7 +64,13 @@ const avatarStyle = css({
   width: 48,
   height: 48,
   borderRadius: 48,
-  backgroundColor: THEME.COLORS.BACKGROUND.DISABLE,
+  backgroundColor: "#D9D9D9",
+});
+
+const profileInfoStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
 });
 
 const nameStyle = css(
@@ -85,32 +87,35 @@ const joinedStyle = css(
   TYPOGRAPHY.SUB["12R"]
 );
 
-const statsRowStyle = css({
+const bottomSectionStyle = css({
   display: "flex",
   flexDirection: "row",
-  alignItems: "stretch",
   justifyContent: "stretch",
-  gap: 16,
+  alignItems: "stretch",
   alignSelf: "stretch",
 });
 
-const statColStyle = css({
+const actionButtonStyle = css({
   display: "flex",
-  flexDirection: "column",
+  flexDirection: "row",
+  justifyContent: "center",
   alignItems: "center",
+  gap: 2,
+  padding: 12,
   flex: 1,
+  backgroundColor: THEME.COLORS.GRAYSCALE.NORMAL,
+  border: "none",
+  borderTop: "1px solid rgba(112, 115, 124, 0.22)",
+  cursor: "pointer",
 });
 
-const statNumberStyle = css(
-  {
-    color: THEME.COLORS.BACKGROUND.WHITE,
-  },
-  TYPOGRAPHY.HEADERS["22B"]
-);
+const logoutButtonStyle = css({
+  borderLeft: "1px solid rgba(112, 115, 124, 0.22)",
+});
 
-const statLabelStyle = css(
+const buttonTextStyle = css(
   {
     color: THEME.COLORS.BACKGROUND.WHITE,
   },
-  TYPOGRAPHY.SUB["12B"]
+  TYPOGRAPHY.SUB["12R"]
 );
