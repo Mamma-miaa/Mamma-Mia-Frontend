99b4a3f4dc36c6638a79efcfe3c1896cf9dd4de6
Jeremy <102432453+shackstack@users.noreply.github.com>
2026-03-20 22:48:33 +0900

    feat: 식당 상세 페이지 배경 이미지 스타일 개선
    - 배경 이미지를 감싸는 래퍼 div 추가
    - 배경 이미지에 dim 효과를 주기 위한 스타일 추가
    - 이미지 위치 및 비율 설정을 개선하여 디자인 일관성 유지

--- src/components/RestaurantDetailBackground.tsx
+++ src/components/RestaurantDetailBackground.tsx
@@ -1,22 +1,48 @@
 import { css } from "@emotion/react"
+import THEME from "@/constants/theme"
 
 const RestaurantDetailBackground = ({ imgUrl }: { imgUrl: string }) => {
   return (
-    <img
-      src={imgUrl}
-      alt="레스토랑 배경 이미지"
-      css={restaurantBackgroundImageStyle}
-    />
+    <div css={restaurantBackgroundWrapperStyle}>
+      <img
+        src={imgUrl}
+        alt="레스토랑 배경 이미지"
+        css={restaurantBackgroundImageStyle}
+      />
+      <div css={restaurantBackgroundDimStyle} />
+    </div>
   )
 }
 
 export default RestaurantDetailBackground
 
 const restaurantBackgroundImageStyle = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
   width: "100%",
   aspectRatio: "375/460",
   objectFit: "cover",
   filter: "blur(140px)",
+  mask: "linear-gradient(180deg, rgba(217, 217, 217, 1) 73%, rgba(115, 115, 115, 0) 100%)",
+  WebkitMask:
+    "linear-gradient(180deg, rgba(217, 217, 217, 1) 73%, rgba(115, 115, 115, 0) 100%)",
+})
+
+const restaurantBackgroundWrapperStyle = css({
+  position: "relative",
+  width: "100%",
+  aspectRatio: "375/460",
+})
+
+const restaurantBackgroundDimStyle = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundColor: THEME.COLORS.GRAYSCALE.STRONG,
+  opacity: 0.2,
   mask: "linear-gradient(180deg, rgba(217, 217, 217, 1) 73%, rgba(115, 115, 115, 0) 100%)",
   WebkitMask:
     "linear-gradient(180deg, rgba(217, 217, 217, 1) 73%, rgba(115, 115, 115, 0) 100%)",
