7c37480bc7df082d2506f4895077c53a62456406
Jeremy <102432453+shackstack@users.noreply.github.com>
2026-03-29 12:38:03 +0900

    Revert "feat: initial commit"
    This reverts commit 6ee9959ad11c5a967ef1c7942512bf0f18ee117c.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +8,6 @@ export default defineConfig({
     react({
       jsxImportSource: "@emotion/react",
     }),
-    tailwindcss(),
     svgr(),
   ],
   resolve: {
